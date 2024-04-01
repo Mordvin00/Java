f7ebff99751987b923dd79e4c3d13833176645f6
Mordvin <mordvin00@gmail.com>
2024-04-01 23:17:55 +0300

    add Les2.OOP & DZ for Les2.OOP

---
+++ Les2.OOPprincAbstractAndInterfaces/ActorBehaviour.java
@@ -0,0 +1,6 @@
+public interface ActorBehaviour {
+    void setMakeOrder(boolean isMakeOrder);
+    void setTakeOrder(boolean isTakeOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+}
